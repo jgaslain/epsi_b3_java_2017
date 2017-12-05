ac84f7f75d4a56154b416a37cade9d217f60b268
Julien GASLAIN <julien.gaslain@gmail.com>
2017-12-05 09:10:02 +0100

    Suppression lignes merge

--- src/main/java/epsi_java_b3/transverse_project/App.java
+++ src/main/java/epsi_java_b3/transverse_project/App.java
@@ -8,13 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World!!!" );
-=======
-        System.out.println( "Hello World!" );
         System.out.println("erwan");
         System.out.println("Damien");
-
->>>>>>> branch 'master' of https://github.com/jgaslain/epsi_b3_java_2017.git
     }
 }
