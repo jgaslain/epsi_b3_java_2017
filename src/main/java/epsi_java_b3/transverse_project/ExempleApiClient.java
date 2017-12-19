d0568e040eb9c3f3af0e92d888754d6a9b7eb9ed
Julien GASLAIN <julien.gaslain@gmail.com>
2017-12-19 09:01:09 +0100

    Ajout appels pour récupération liste boitiers et photos

--- src/main/java/epsi_java_b3/transverse_project/ExempleApiClient.java
+++ src/main/java/epsi_java_b3/transverse_project/ExempleApiClient.java
@@ -1,7 +1,14 @@
 package epsi_java_b3.transverse_project;
 
 import epsi.client_copeeks_v3.CopeeksClient;
+import io.swagger.client.api.PicturesApi;
 import io.swagger.client.api.SensorsApi;
+import io.swagger.client.api.UsersApi;
+import io.swagger.client.model.BoxWakeUp;
+import io.swagger.client.model.BoxWakeUpList;
+import io.swagger.client.model.Picture;
+import io.swagger.client.model.PicturesCountBetweenDates;
+import io.swagger.client.model.PicturesList;
 import io.swagger.client.model.SensorValue;
 import io.swagger.client.model.SensorValuesList;
 
@@ -51,6 +58,27 @@ public class ExempleApiClient {
 			}
 			System.out.println(temperatureMin);
 			
+
+			// Récupération des boitiers auquel l'utilisateur a accès
+			
+			BoxWakeUpList boxList = new UsersApi(CopeeksClient.get()).getBoxList(null);
+			
+			for (BoxWakeUp box : boxList.getAvailableBoxes()) {
+				System.out.println(box.toString());
+			}
+			
+			// Récupération des photos pour une période donnée
+			PicturesCountBetweenDates count = new PicturesApi(CopeeksClient.get()).getPicturesBetweenDatesCount("PEEK_BREHOU_2", "2017-12-15", "2017-12-18", null);
+			System.out.println(count.getTotalPictures());
+			
+			PicturesList pictures = new PicturesApi(CopeeksClient.get()).getPicturesByDay("PEEK_BREHOU_2", "2017-12-18", null);
+			
+			for (Picture picture : pictures.getPicturesList()) {
+				System.out.println(picture.toString());
+			}
+
+
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
